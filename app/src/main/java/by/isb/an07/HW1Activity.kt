d7e23026864c3296e4845fb913fee2169dcf1f39
Berikz80 <berikz@gmail.com>
2021-04-09 23:27:25 +0300

    added picasso

--- app/src/main/java/by/isb/an07/HW1Activity.kt
+++ app/src/main/java/by/isb/an07/HW1Activity.kt
@@ -2,10 +2,22 @@ package by.isb.an07
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
 
 class HW1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hw1)
+
+        val imageBox = findViewById<ImageView>(R.id.image_box)
+        val inputUrl = findViewById<EditText>(R.id.input_url)
+        val buttonShow = findViewById<Button>(R.id.button_show)
+
+        buttonShow.setOnClickListener {
+            Picasso.get().load(inputUrl.text.toString()).into(imageBox)
+        }
     }
 }
