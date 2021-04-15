8320d8b8095ece40d12ec2dc8634b9b6d4e6f877
Igor <berikz@gmail.com>
2021-04-16 00:13:11 +0300

    add logic

--- app/src/main/java/by/isb/an07/HW3Activity.kt
+++ app/src/main/java/by/isb/an07/HW3Activity.kt
@@ -1,10 +1,12 @@
 package by.isb.an07
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.activity_hw3.*
 
-class HW3Activity:AppCompatActivity() {
+class HW3Activity : AppCompatActivity() {
 
     lateinit var viewModel: HW3ViewModel
 
@@ -14,9 +16,23 @@ class HW3Activity:AppCompatActivity() {
 
         viewModel = ViewModelProvider(this).get(HW3ViewModel::class.java)
 
-        viewModel.init()
+        viewModel.myRegions[0].observe(this) {
+            text1_1.text = it.corn.toString()
+            text1_2.text = it.potato.toString()
+            text1_3.text = it.cabbage.toString()
+        }
 
+        viewModel.myRegions[1].observe(this) {
+            text2_1.text = it.corn.toString()
+            text2_2.text = it.potato.toString()
+            text2_3.text = it.cabbage.toString()
+        }
 
+        viewModel.myRegions[2].observe(this) {
+            text3_1.text = it.corn.toString()
+            text3_2.text = it.potato.toString()
+            text3_3.text = it.cabbage.toString()
+        }
 
     }
 }
--- app/src/main/java/by/isb/an07/HW3ViewModel.kt
+++ app/src/main/java/by/isb/an07/HW3ViewModel.kt
@@ -2,17 +2,45 @@ package by.isb.an07
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 class HW3ViewModel : ViewModel() {
 
     val myRegions = arrayOf(MutableLiveData<Region>())
 
-    fun init() {
+    val random = Random(12345)
+
+    fun startDataLoading() {
+
+        fun loadInt(): Int {
+            var r = 0
+            viewModelScope.launch {
+                delay(random.nextLong(5000))
+                r = random.nextLong(100).toInt()
+            }
+            return r
+        }
         myRegions[0] = MutableLiveData(Region("Гомельская область"))
         myRegions[1] = MutableLiveData(Region("Могилевская область"))
         myRegions[2] = MutableLiveData(Region("Брестская область"))
-    }
 
+        while (true) {
+
+            for (i in 0..2) {
+                myRegions[i].value?.corn?.plus(loadInt())
+                myRegions[i].value?.potato?.plus(loadInt())
+                myRegions[i].value?.cabbage?.plus(loadInt())
+            }
+
+        }
+
+
+
+
+    }
 
 
 }
