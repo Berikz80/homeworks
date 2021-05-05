238395d89cbe4d5e845026bf9b0469fd75f5ac38
Igor <berikz@gmail.com>
2021-05-05 21:06:36 +0300

    start hw6

--- app/src/main/java/by/isb/an07/MainActivity.kt
+++ app/src/main/java/by/isb/an07/MainActivity.kt
@@ -29,5 +29,9 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.hw5_button).setOnClickListener {
             startActivity(Intent(this,HW5Activity::class.java))
         }
+
+        findViewById<Button>(R.id.hw6_button).setOnClickListener {
+            startActivity(Intent(this,HW6Activity::class.java))
+        }
     }
 }
