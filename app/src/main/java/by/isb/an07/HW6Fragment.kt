0cf0fc43e6a91121e08b496ea14dde5fe1d2d23b
Igor <berikz@gmail.com>
2021-05-05 21:38:03 +0300

    add fragments

---
+++ app/src/main/java/by/isb/an07/HW6Fragment.kt
@@ -0,0 +1,18 @@
+package by.isb.an07
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class HW6Fragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_hw6, container, false)
+    }
+
+}
