24278c51be95b838fadcde86de5a07f6128f6645
Igor <berikz@gmail.com>
2021-05-05 22:35:59 +0300

    add animation

--- app/src/main/java/by/isb/an07/CardFragment.kt
+++ app/src/main/java/by/isb/an07/CardFragment.kt
@@ -1,10 +1,12 @@
 package by.isb.an07
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
 
 class CardFragment : Fragment() {
 
@@ -14,6 +16,16 @@ class CardFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_card, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        view.findViewById<ImageView>(R.id.image_orden)
+            .startAnimation(AnimationUtils.loadAnimation(context, R.anim.large_small))
+
     }
 
 }
--- app/src/main/java/by/isb/an07/HW6Fragment.kt
+++ app/src/main/java/by/isb/an07/HW6Fragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 
 class HW6Fragment : Fragment() {
 
@@ -13,6 +15,14 @@ class HW6Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_hw6, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button_show_card).setOnClickListener {
+            findNavController().navigate(R.id.show_cardFragment)
+        }
     }
 
 }
