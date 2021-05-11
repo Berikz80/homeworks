cab81b02fa8243d55e22b7d74be0de65b96fc009
Igor <berikz@gmail.com>
2021-05-11 20:42:45 +0300

    add hw7 live find edit text

--- app/src/main/java/by/isb/an07/HW7Activity.kt
+++ app/src/main/java/by/isb/an07/HW7Activity.kt
@@ -1,22 +1,26 @@
 package by.isb.an07
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
-import by.isb.an07.database.entity.Product
 import by.isb.an07.recycler.ProductAdapter
+
 
 class HW7Activity : AppCompatActivity() {
 
-    lateinit var viewModel : HW7ViewModel
+    lateinit var viewModel: HW7ViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hw7)
 
-        viewModel = ViewModelProvider.AndroidViewModelFactory(application).create(HW7ViewModel::class.java)
+        viewModel =
+            ViewModelProvider.AndroidViewModelFactory(application).create(HW7ViewModel::class.java)
+        val recycler = findViewById<RecyclerView>(R.id.recycler)
 
 //        viewModel.insert(Product("Milk",100,""))
 //        viewModel.insert(Product("Bread",200,""))
@@ -24,14 +28,32 @@ class HW7Activity : AppCompatActivity() {
 
         viewModel.loadAllProduct()
 
-
         viewModel.productList.observe(this) {
             val productsAdapter = viewModel.productList.value?.let { ProductAdapter(it) }
-
-            val recycler = findViewById<RecyclerView>(R.id.recycler)
             recycler.adapter = productsAdapter
         }
 
+        findViewById<EditText>(R.id.quick_find).addTextChangedListener(
+            object : TextWatcher {
+                override fun afterTextChanged(s: Editable) {
+                    val productsAdapter = viewModel.productList.value?.filter {
+                        it.name.contains(s, true)
+                    }?.let { ProductAdapter(it) }
+                    recycler.adapter = productsAdapter
+                }
+
+                override fun beforeTextChanged(
+                    s: CharSequence, start: Int, count: Int,
+                    after: Int
+                ) {
+                }
+
+                override fun onTextChanged(
+                    s: CharSequence, start: Int, before: Int,
+                    count: Int
+                ) {
+                }
+            })
 
 
     }
